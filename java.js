235cc6fc1a970f5aa4744d3b49fce33500544693
lucas-lulu <lucasnoivas4@gmail.com>
2026-01-20 10:50:41 -0300

    mudanças

--- java.js
+++ java.js
@@ -1,13 +1,9 @@
 const menu = document.querySelector('#mobile-menu');
     const menuLinks = document.querySelector('.menu');
-
-    // Abre/Fecha o menu ao clicar no hamburguer
     menu.addEventListener('click', function() {
         menu.classList.toggle('is-active');
         menuLinks.classList.toggle('ativo');
     });
-
-    // Fecha o menu automaticamente ao clicar em um link (para rolar até a sessão)
     document.querySelectorAll('.menu a').forEach(link => {
         link.addEventListener('click', () => {
             menu.classList.remove('is-active');
